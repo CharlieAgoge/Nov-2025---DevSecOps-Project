a171d6f3995bc83ec0bdfc8baa6b9773489b5d4f
charlie <charlie@hotmail.com>
2026-01-25 11:53:41 +0000

    update

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
+resource "aws_s3_bucket" "terraform2333" {
+  bucket = "terraform2333"
+}
+resource "aws_s3_bucket" "terraform23333" {
+  bucket = "terraform23333"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform2333"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
